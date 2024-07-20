4cffe97e6e9ef2ced1b9209f816f63e0a492f075
Ab. Raqib <raqibramaki3.14@gmail.com>
2024-07-19 18:15:51 -0700

    Day 32: Add a static plot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import plotly.express as px
 
+# Add widgets
 st.title("Weather Forecast for Next Days")
 city = st.text_input("Place:")
 days = st.slider("Forecast Days: ", min_value=1, max_value=5,
@@ -9,3 +11,16 @@ option = st.selectbox("Select data to view: ",
                       options=("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {city}")
+
+# Get data
+def get_data():
+    dates = ["2024-19-7", "2024-20-7", "2024-21-7"]
+    temperatures = [23, 20, 29]
+    return dates, temperatures
+
+
+d, t = get_data()
+
+# Create a plot
+figure = px.line(x=d, y=t, labels={'x': "Date", 'y': "Temperature"})
+st.plotly_chart(figure)
