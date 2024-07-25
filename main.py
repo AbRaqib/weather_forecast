409fa1d84980d6801886ceeb035efe9af03f8290
Ab. Raqib <raqibramaki3.14@gmail.com>
2024-07-25 01:43:19 -0700

    Day 33: Solving 2 issues: 1. dividing temperatures by 10 2. handling the error in case if user was to enter a non-existing city name.

--- main.py
+++ main.py
@@ -9,24 +9,27 @@ days = st.slider("Forecast Days: ", min_value=1, max_value=5,
                  help="Choose the number of forecast days.")
 option = st.selectbox("Select data to view: ",
                       options=("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {city}")
+try:
+    st.subheader(f"{option} for the next {days} days in {city}")
 
-if city:
-    # Get data
-    filtered_data = get_data(city, days)
+    if city:
+        # Get data
+        filtered_data = get_data(city, days)
 
-    # Create a plot for temperature
-    if option == "Temperature":
-        temperatures = [dic['main']['temp'] for dic in filtered_data]
-        times = [dic['dt_txt'] for dic in filtered_data]
-        figure = px.line(x=times, y=temperatures,
-                         labels={'x': "Date", 'y': "Temperature"})
-        st.plotly_chart(figure)
+        # Create a plot for temperature
+        if option == "Temperature":
+            temperatures = [dic['main']['temp'] / 10 for dic in filtered_data]
+            times = [dic['dt_txt'] for dic in filtered_data]
+            figure = px.line(x=times, y=temperatures,
+                             labels={'x': "Date", 'y': "Temperature"})
+            st.plotly_chart(figure)
 
-    # Render images for sky condition
-    if option == "Sky":
-        sky_conditions = [dic['weather'][0]['main'] for dic in filtered_data]
-        translation = {'Clear': 'images/clear.png', 'Rain': 'images/rain.png',
-                       'Snow': 'images/snow.png', 'Clouds': 'images/cloud.png'}
-        image_paths = [translation[condition] for condition in sky_conditions]
-        st.image(image_paths, width=100)
+        # Render images for sky condition
+        if option == "Sky":
+            sky_conditions = [dic['weather'][0]['main'] for dic in filtered_data]
+            translation = {'Clear': 'images/clear.png', 'Rain': 'images/rain.png',
+                           'Snow': 'images/snow.png', 'Clouds': 'images/cloud.png'}
+            image_paths = [translation[condition] for condition in sky_conditions]
+            st.image(image_paths, width=100)
+except KeyError:
+    st.info("Oops! You entered an non-existing city name.")
