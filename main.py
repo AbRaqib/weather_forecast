7cd74550e9422927319a4b83fab5741067c4b74e
Ab. Raqib <raqibramaki3.14@gmail.com>
2024-07-25 01:32:57 -0700

    Day 33: Render the temperature data in plot, and sky condition images in web page.

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ from backend import get_data
 # Add widgets
 st.title("Weather Forecast for Next Days")
 city = st.text_input("Place:")
-days = st.slider("Forecast Days: ", min_value=1
-                 , max_value=5,
+days = st.slider("Forecast Days: ", min_value=1, max_value=5,
                  help="Choose the number of forecast days.")
-
 option = st.selectbox("Select data to view: ",
                       options=("Temperature", "Sky"))
-
 st.subheader(f"{option} for the next {days} days in {city}")
 
-# Get data
-data = get_data(city, days, option)
+if city:
+    # Get data
+    filtered_data = get_data(city, days)
 
-# Create a plot
-figure = px.line(x=d, y=t, labels={'x': "Date", 'y': "Temperature"})
-st.plotly_chart(figure)
+    # Create a plot for temperature
+    if option == "Temperature":
+        temperatures = [dic['main']['temp'] for dic in filtered_data]
+        times = [dic['dt_txt'] for dic in filtered_data]
+        figure = px.line(x=times, y=temperatures,
+                         labels={'x': "Date", 'y': "Temperature"})
+        st.plotly_chart(figure)
+
+    # Render images for sky condition
+    if option == "Sky":
+        sky_conditions = [dic['weather'][0]['main'] for dic in filtered_data]
+        translation = {'Clear': 'images/clear.png', 'Rain': 'images/rain.png',
+                       'Snow': 'images/snow.png', 'Clouds': 'images/cloud.png'}
+        image_paths = [translation[condition] for condition in sky_conditions]
+        st.image(image_paths, width=100)
