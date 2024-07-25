15cafac29dee3e6a941e9a8eba0d3e47ef1dfe40
Ab. Raqib <raqibramaki3.14@gmail.com>
2024-07-25 00:33:27 -0700

    Day 33: Get the temperature & sky condition in a static data in backend.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 # Add widgets
 st.title("Weather Forecast for Next Days")
 city = st.text_input("Place:")
-days = st.slider("Forecast Days: ", min_value=1, max_value=5,
+days = st.slider("Forecast Days: ", min_value=1
+                 , max_value=5,
                  help="Choose the number of forecast days.")
 
 option = st.selectbox("Select data to view: ",
@@ -13,13 +15,7 @@ option = st.selectbox("Select data to view: ",
 st.subheader(f"{option} for the next {days} days in {city}")
 
 # Get data
-def get_data():
-    dates = ["2024-19-7", "2024-20-7", "2024-21-7"]
-    temperatures = [23, 20, 29]
-    return dates, temperatures
-
-
-d, t = get_data()
+data = get_data(city, days, option)
 
 # Create a plot
 figure = px.line(x=d, y=t, labels={'x': "Date", 'y': "Temperature"})
